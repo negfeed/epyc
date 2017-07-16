64478195a55cd838052731ec8653032a8a42f6b4
Mohammad Shamma <mohammadshamma@gmail.com>
2017-07-16 03:51:49 -0700

    Remove unnecessary import.

--- src/pages/game-results/game-results.ts
+++ src/pages/game-results/game-results.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-import { HomePage } from '../home/home';
-
 @Component({
   selector: 'page-game-results',
   templateUrl: 'game-results.html',

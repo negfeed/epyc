307722096a9d23a8659458294899a5ac10051826
Mohammad Shamma <mohammadshamma@gmail.com>
2018-01-01 01:52:17 -0800

    Implements the wait game to end page (fixes #24).

--- src/pages/wait-game-to-end/wait-game-to-end.ts
+++ src/pages/wait-game-to-end/wait-game-to-end.ts
@@ -1,7 +1,20 @@
 import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavParams, Navbar } from 'ionic-angular';
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
 
 import { GameNavigationController } from '../../providers/game-navigation-controller/game-navigation-controller';
+import { GameModel, GameModelInterface, GameThread, GameAtomState } from '../../providers/game-model/game-model';
+
+interface DisplayThread {
+  threadNumber: number,
+  completedStepsCount: number,
+  totalStepsCount: number,
+}
+
+interface DisplayThreads {
+  [index: number]: DisplayThread;
+}
 
 @IonicPage()
 @Component({
@@ -11,23 +24,46 @@ import { GameNavigationController } from '../../providers/game-navigation-contro
 export class WaitGameToEndPage {
 
   private gameKey: string;
+  private ngUnsubscribe: Subject<void> = null;
+  private threads: Observable<DisplayThreads> = null;
 
   @ViewChild(Navbar) navbar: Navbar
 
   constructor(
       navParams: NavParams,
-      private gameNavCtrl: GameNavigationController) {
+      private gameNavCtrl: GameNavigationController,
+      private gameModel: GameModel) {
     this.gameKey = navParams.get('gameKey');
   }
 
   ionViewDidEnter() {
     console.log('ionViewDidEnter WaitGameToEndPage');
+    this.ngUnsubscribe = new Subject<void>();
+    let gameInstanceObservable = this.gameModel.loadInstance(this.gameKey).takeUntil(this.ngUnsubscribe);
+    this.threads = gameInstanceObservable.map((gameInstance: GameModelInterface) => {
+      let threads: Array<DisplayThread> = [];
+      for (let index = 0; index < gameInstance.threads.length; ++index) {
+        let gameThread: GameThread = gameInstance.threads[index];
+        let completedStepsCount: number = 0;
+        for (let atomIndex = 0; atomIndex < gameThread.gameAtoms.length; ++atomIndex) {
+          if (gameThread.gameAtoms[atomIndex].state == GameAtomState.DONE) completedStepsCount++;
+        }
+        threads.push({
+          threadNumber: index + 1,
+          completedStepsCount: completedStepsCount,
+          totalStepsCount: gameThread.gameAtoms.length,
+        });
+      }
+      return threads;
+    });
     this.navbar.backButtonClick = () => this.backButtonAction();
     this.gameNavCtrl.observeAndNavigateToNextPage(this.gameKey, 'WaitGameToEndPage');
   }
 
   ionViewWillLeave() {
     console.log('ionViewWillLeave WaitGameToEndPage');
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
     this.gameNavCtrl.cancelObserveAndNavigateToNextPage();
   }
 
